93d1c67816096ade8363f5fade0ffdbad61fe631
sunshineDrizzle <954830460@qq.com>
2018-08-31 04:23:31 -0400

    Refine: batch processing of cifti merge through wb_command

--- bin/cifti_merge_wb_cmd.py
+++ bin/cifti_merge_wb_cmd.py
@@ -1,15 +1,20 @@
 if __name__ == '__main__':
+    """
+    NOTE1!!! wb_command -cifti-merge may load all source cifti files into memory.
+    NOTE2!!! wb_command -cifti-merge may destroy partial data when merge too many files.
+    I do have experienced the two phenomena above.
+    """
     import os
-    from subprocess import Popen
+    from subprocess import run
 
     from commontool.algorithm.string import get_strings_by_filling
 
     column = '20'
-    target_dir = '/home/bnucnl/cxy_workingdir/S1200_face_avg/'
-    filler_file0 = os.path.join(target_dir, 'sessid')
-    string = '/s3/hcp/{0}/MNINonLinear/Results/tfMRI_WM/tfMRI_WM_hp200_s4_level2_MSMAll.feat/' \
+    target_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/rFFA_clustering/data/S1200_face-avg'
+    filler_file0 = os.path.join(target_dir, 'sessid_all')
+    string = '/nfs/s2/userhome/chenxiayu/workingdir/data/HCP/S1200_WM_all_cope/' \
              '{0}_tfMRI_WM_level2_hp200_s4_MSMAll.dscalar.nii'
-    out_path = os.path.join(target_dir, 'S1200.All.face-avg_level2_zstat_hp200_s4_MSMAll.dscalar.nii')
+    out_path = os.path.join(target_dir, 'S1200.1080.face-avg_level2_zstat_hp200_s4_MSMAll.dscalar.nii')
 
     with open(filler_file0) as f0:
         fillers_c0 = f0.read().splitlines()
@@ -26,8 +31,8 @@ if __name__ == '__main__':
         merge_cmd.extend(['-cifti', fpath, '-column', column])
 
     log_file.writelines('Running: ' + ' '.join(merge_cmd) + '\n')
-    Popen(merge_cmd,
-          stderr=open(os.path.join(target_dir, 'cifti_merge_stderr'), 'w+'),
-          stdout=open(os.path.join(target_dir, 'cifti_merge_stdout'), 'w+'))
+    run(merge_cmd,
+        stderr=open(os.path.join(target_dir, 'cifti_merge_stderr'), 'w+'),
+        stdout=open(os.path.join(target_dir, 'cifti_merge_stdout'), 'w+'))
     log_file.write('done')
     log_file.close()
