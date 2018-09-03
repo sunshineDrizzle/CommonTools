58b20f98ee04eab0a62939434d9a30767a9b2004
sunshineDrizzle <954830460@qq.com>
2018-09-02 23:01:45 -0400

    NF: calculate overlap

---
+++ commontool/algorithm/tests/test_tool.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from commontool.algorithm.tool import calc_overlap
+
+
+def test_calc_overlap():
+    c1 = [1, 2, 3, 4]
+    c2 = (3, 4, 5, 6)
+    print(calc_overlap(c1, c2))
+
+    array1 = np.array([1, 2, 3, 2])
+    array2 = np.array([4, 5, 6, 4])
+    print(calc_overlap(array1, array2, 2, 4))
+
+
+test_calc_overlap()
---
+++ commontool/algorithm/tool.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+
+def _overlap(c1, c2, index='dice'):
+    """
+    Calculate overlap between two collections
+
+    Parameters
+    ----------
+    c1, c2 : collection (list | tuple | set | 1-D array etc.)
+    index : string ('dice' | 'percent')
+        This parameter is used to specify index which is used to measure overlap.
+
+    Return
+    ------
+    overlap : float
+        The overlap between c1 and c2
+    """
+    set1 = set(c1)
+    set2 = set(c2)
+    intersection_num = float(len(set1 & set2))
+    try:
+        if index == 'dice':
+            total_num = len(set1 | set2) + intersection_num
+            overlap = 2.0 * intersection_num / total_num
+        elif index == 'percent':
+            overlap = 1.0 * intersection_num / len(set1)
+        else:
+            raise Exception("Only support 'dice' and 'percent' as overlap indices at present.")
+    except ZeroDivisionError as e:
+        print(e)
+        overlap = np.nan
+    return overlap
+
+
+def calc_overlap(data1, data2, label1=None, label2=None, index='dice'):
+    """
+    Calculate overlap between two sets.
+    The sets are acquired from data1 and data2 respectively.
+
+    Parameters
+    ----------
+    data1, data2 : collection or numpy array
+        label1 is corresponding with data1
+        label2 is corresponding with data2
+    label1, label2 : None or labels
+        If label1 or label2 is None, the corresponding data is supposed to be
+        a collection of members such as vertices and voxels.
+        If label1 or label2 is a label, the corresponding data is always a numpy array with same shape and meaning.
+        And we will acquire set1 elements whose labels are equal to label1 from data1
+        and set2 elements whose labels are equal to label2 from data2.
+    index : string ('dice' | 'percent')
+        This parameter is used to specify index which is used to measure overlap.
+
+    Return
+    ------
+    overlap : float
+        The overlap of data1 and data2
+    """
+    if label1 is not None:
+        positions1 = np.where(data1 == label1)
+        data1 = list(zip(*positions1))
+
+    if label2 is not None:
+        positions2 = np.where(data2 == label2)
+        data2 = list(zip(*positions2))
+
+    # calculate overlap
+    overlap = _overlap(data1, data2, index)
+
+    return overlap
