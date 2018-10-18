d23249674ed6e66aeab4f7814eaa18e3c4d3bfa8
sunshineDrizzle <954830460@qq.com>
2018-10-18 23:34:48 +0800

    NF: do clustering with gap statistic assessment

--- commontool/algorithm/tests/test_tool.py
+++ commontool/algorithm/tests/test_tool.py
@@ -32,3 +32,61 @@ def test_box_sampling():
     ax.add_patch(rect)
     fig.tight_layout()
     plt.show()
+
+
+def test_gap_statistic():
+    from commontool.algorithm.tool import gap_statistic, uniform_box_sampling
+    from matplotlib.patches import Rectangle
+
+    cluster1_x = np.random.normal(2, 0.2, 100)
+    cluster1_y = np.random.normal(1, 0.2, 100)
+    cluster1 = np.c_[cluster1_x, cluster1_y]
+    cluster2_x = np.random.normal(3, 0.2, 100)
+    cluster2_y = np.random.normal(3, 0.2, 100)
+    cluster2 = np.c_[cluster2_x, cluster2_y]
+    cluster3_x = np.random.normal(5, 0.2, 100)
+    cluster3_y = np.random.normal(1, 0.2, 100)
+    cluster3 = np.c_[cluster3_x, cluster3_y]
+    data = np.r_[cluster1, cluster2, cluster3]
+
+    cluster_nums = range(1, 11)
+    labels_list, Wks, Wks_refs_log_mean, gaps, s, k_selected = gap_statistic(data, cluster_nums)
+
+    minimums = np.atleast_2d(np.min(data, 0))
+    maximums = np.atleast_2d(np.max(data, 0))
+    bounding_box = np.r_[minimums, maximums]
+    dists = bounding_box[1] - bounding_box[0]
+    rect = Rectangle(bounding_box[0], dists[0], dists[1], edgecolor='r', facecolor='none')
+    uniform_samples = uniform_box_sampling(data.shape[0], bounding_box)
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_xlim([0, 6])
+    ax1.set_ylim([0, 5])
+    ax1.scatter(data[:, 0], data[:, 1])
+    ax1.scatter(uniform_samples[:, 0], uniform_samples[:, 1], c='r')
+    ax1.add_patch(rect)
+    fig1.tight_layout()
+
+    x = np.arange(len(cluster_nums))
+    plt.figure()
+    plt.plot(x, Wks, 'b.-')
+    plt.xlabel('#cluster')
+    plt.ylabel(('W\u2096'))
+    plt.xticks(x, cluster_nums)
+
+    Wks_log = np.log(Wks)
+    plt.figure()
+    plt.plot(x, Wks_log, 'b.-')
+    plt.plot(x, Wks_refs_log_mean, 'r.-')
+    plt.xlabel('#cluster')
+    plt.ylabel('log(W\u2096)')
+    plt.xticks(x, cluster_nums)
+
+    plt.figure()
+    plt.plot(x, gaps, 'b.-')
+    plt.fill_between(x, gaps-s, gaps+s, alpha=0.5)
+    plt.xlabel('#cluster')
+    plt.ylabel('gap statistic')
+    plt.xticks(x, cluster_nums)
+
+    plt.show()
--- commontool/algorithm/tool.py
+++ commontool/algorithm/tool.py
@@ -148,6 +148,93 @@ def elbow_score(X, labels, metric='euclidean', type=('inner', 'standard')):
     return score
 
 
+def gap_statistic(X, cluster_nums, ref_num=10, cluster_method=None):
+    """
+    do clustering with gap statistic assessment according to (Tibshirani et al., 2001b)
+    https://blog.csdn.net/baidu_17640849/article/details/70769555
+    https://datasciencelab.wordpress.com/tag/gap-statistic/
+    https://github.com/milesgranger/gap_statistic
+
+    :param X: array, shape = (n_samples, n_features)
+        a feature array
+    :param cluster_nums: a iterator of integers
+        Each integer is the number of clusters to try on the data.
+    :param ref_num: integer
+        The number of random reference data sets used as inertia reference to actual data.
+    :param cluster_method: callable
+        The cluster method to do clustering on the feature array. And the method returns
+        labels_list (cluster results of each cluster_num in cluster_nums).
+        If is None, a default K-means method will be used.
+
+    :return: labels_list: list
+        cluster results of each cluster_num in cluster_nums
+    :return: Wks: array, shape = (len(cluster_nums),)
+        within-cluster dispersion of each cluster_num clustering on the feature array X
+    :return: Wks_refs_log_mean: array, shape = (len(cluster_nums),)
+        mean within-cluster dispersion of each cluster_num clustering on ref_num reference data sets
+    :return: gaps: array, shape = (len(cluster_nums),)
+        Wks_refs_log_mean - np.log(Wks)
+    :return: s: array, shape = (len(cluster_nums),)
+        I think elements in s can be regarded as standard errors of gaps.
+    :return: k_selected: integer
+        cluster k_selected clusters on X may be the best choice
+    """
+    if cluster_method is None:
+        def k_means(data, cluster_nums):
+            """
+            http://scikit-learn.org/stable/modules/clustering.html#k-means
+            """
+            from sklearn.cluster import KMeans
+
+            labels_list = []
+            for cluster_num in cluster_nums:
+                kmeans = KMeans(cluster_num, random_state=0, n_init=10).fit(data)
+                labels_list.append(kmeans.labels_ + 1)
+                print('KMeans finished: {}'.format(cluster_num))
+            return labels_list
+
+        cluster_method = k_means
+
+    print('Start: calculate W\u2096s')
+    Wks = []
+    labels_list = cluster_method(X, cluster_nums)
+    for labels in labels_list:
+        Wks.append(elbow_score(X, labels))
+    Wks = np.array(Wks)
+    Wks_log = np.log(Wks)
+    print('Finish: calculate W\u2096s')
+
+    print("Start: calculate references' W\u2096s")
+    Wks_refs_log = []
+    minimums = np.atleast_2d(np.min(X, axis=0))
+    maximums = np.atleast_2d(np.max(X, axis=0))
+    bounding_box = np.r_[minimums, maximums]
+    for i in range(ref_num):
+        X_ref = uniform_box_sampling(X.shape[0], bounding_box)
+        labels_list_ref = cluster_method(X_ref, cluster_nums)
+        Wks_ref_log = []
+        for labels in labels_list_ref:
+            Wks_ref_log.append(np.log(elbow_score(X_ref, labels)))
+        Wks_refs_log.append(Wks_ref_log)
+        print('Finish reference: {}/{}'.format(i+1, ref_num))
+    print("Finish: calculate references' W\u2096s")
+
+    print('Start: calculate gaps')
+    Wks_refs_log = np.array(Wks_refs_log)
+    Wks_refs_log_mean = np.mean(Wks_refs_log, axis=0)
+    Wks_refs_log_std = np.std(Wks_refs_log, axis=0)
+    gaps = Wks_refs_log_mean - Wks_log
+    print('Finish: calculate gaps')
+
+    print('Start: select optimal k')
+    s = Wks_refs_log_std * np.sqrt(1 + 1.0 / ref_num)
+    idx_selected = np.where(gaps[:-1] >= gaps[1:] - s[1:])[0][0]
+    k_selected = cluster_nums[idx_selected]
+    print('Finish: select optimal k')
+
+    return labels_list, Wks, Wks_refs_log_mean, gaps, s, k_selected
+
+
 # --------sampling--------
 def uniform_box_sampling(n_sample, bounding_box=((0,), (1,))):
     """
