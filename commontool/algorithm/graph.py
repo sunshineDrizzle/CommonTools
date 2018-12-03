7a1b6c745a4144b9ab836eb53582a71ec9556f99
sunshineDrizzle <954830460@qq.com>
2018-12-03 10:47:29 +0800

    NF: connectivity_grow

--- commontool/algorithm/graph.py
+++ commontool/algorithm/graph.py
@@ -241,3 +241,34 @@ def bfs(edge_list, start, end, deep_limit=np.inf):
                 path_queue.append(tmp_path + [link_node])
 
     return []
+
+
+def connectivity_grow(seeds_id, edge_list):
+    """
+    Find all vertices for each group of initial seeds.
+
+    Parameters
+    ----------
+    seeds_id : list
+        Its elements are also list, called sub-list,
+        each sub-list contains a group of seed vertices which are used to initialize a evolving region.
+        Different sub-list initializes different connected region.
+    edge_list : dict | list
+        The indices are vertices of a graph.
+        One index's corresponding element is a collection of vertices which connect with the index.
+    Return
+    ------
+    connected_regions : list
+        Its elements are set, each set contains all vertices which connect with corresponding seeds.
+    """
+    connected_regions = [set(seeds) for seeds in seeds_id]
+
+    for idx, region in enumerate(connected_regions):
+        outmost_vtx = region.copy()
+        while outmost_vtx:
+            print('connected region{idx} size: {size}'.format(idx=idx, size=len(region)))
+            region_old = region.copy()
+            for vtx in outmost_vtx:
+                region.update(edge_list[vtx])
+            outmost_vtx = region.difference(region_old)
+    return connected_regions
