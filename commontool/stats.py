96db0a3c2e280825ff3c8791d71e62bbeef76add
sunshineDrizzle <954830460@qq.com>
2020-03-15 00:04:23 +0800

    NF: implement two-way ANOVA

---
+++ commontool/stats.py
@@ -0,0 +1,59 @@
+from statsmodels.formula.api import ols
+from statsmodels.stats.anova import anova_lm
+
+
+class ANOVA:
+    """
+    Methods:
+    -------
+    eta_squared, omega_squared:
+        As a Psychologist most of the journals we publish in requires to report effect sizes.
+        Common software, such as, SPSS have eta squared as output.
+        However, eta squared is an overestimation of the effect.
+        To get a less biased effect size measure we can use omega squared.
+        The two methods adds eta squared and omega squared to the DataFrame that contains the ANOVA table.
+
+    References:
+    ----------
+    1. http://www.pybloggers.com/2016/03/three-ways-to-do-a-two-way-anova-with-python/
+    """
+    def two_way(self, data, dep_var, factor1, factor2):
+        """
+        two-way ANOVA
+
+        Parameters:
+        ----------
+        data: DataFrame
+            Contains at least 3 columns that are 'dependent variable', 'factor1', and 'factor2' respectively.
+        dep_var: str
+            Name of the 'dependent variable' column.
+        factor1: str
+            Name of the 'factor1' column.
+        factor2: str
+            Name of the 'factor2' column.
+
+        Return:
+        aov_table: DataFrame
+            ANOVA table
+        """
+        formula = '{0} ~ C({1}) + C({2}) + C({1}):C({2})'.format(dep_var, factor1, factor2)
+        print('formula:', formula)
+        model = ols(formula, data).fit()
+        aov_table = anova_lm(model, typ=2)
+        self.eta_squared(aov_table)
+        self.omega_squared(aov_table)
+
+        return aov_table
+
+    @staticmethod
+    def eta_squared(aov):
+        aov['eta_sq'] = 'NaN'
+        aov['eta_sq'] = aov[:-1]['sum_sq'] / sum(aov['sum_sq'])
+        return aov
+
+    @staticmethod
+    def omega_squared(aov):
+        mse = aov['sum_sq'][-1] / aov['df'][-1]
+        aov['omega_sq'] = 'NaN'
+        aov['omega_sq'] = (aov[:-1]['sum_sq'] - (aov[:-1]['df'] * mse)) / (sum(aov['sum_sq']) + mse)
+        return aov
---
+++ commontool/tests/test_stats.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+from commontool import stats as ct_stats
+
+
+class TestANOVA:
+    def test_two_way(self):
+        # ground truth
+        sum_sq = [205.350000, 2426.434333, 108.319000, 712.106000]
+        df = [1, 2, 2, 54]
+        F = [15.571979, 91.999965, 4.106991]
+        PR = [2.311828e-04, 4.046291e-18, 2.186027e-02]
+        eta_sq = [0.059484, 0.702864, 0.031377]
+        omega_sq = [0.055452, 0.692579, 0.023647]
+
+        # test
+        data = pd.read_csv('../data/ToothGrowth.csv')
+        anova = ct_stats.ANOVA()
+        aov_tabel = anova.two_way(data, 'len', 'supp', 'dose')
+        np.testing.assert_almost_equal(np.asarray(aov_tabel['sum_sq']), sum_sq, 6)
+        assert df == aov_tabel['df'].to_list()
+        np.testing.assert_almost_equal(np.asarray(aov_tabel['F'][:3]), F, 6)
+        np.testing.assert_almost_equal(np.asarray(aov_tabel['PR(>F)'][:3]), PR, 6)
+        np.testing.assert_almost_equal(np.asarray(aov_tabel['eta_sq'][:3]), eta_sq, 6)
+        np.testing.assert_almost_equal(np.asarray(aov_tabel['omega_sq'][:3]), omega_sq, 6)
