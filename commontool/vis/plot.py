716a3b18656c5c4fae999f21609ffa502841e273
sunshineDrizzle <954830460@qq.com>
2020-08-07 13:48:18 +0800

    NF: polyfit_plot

---
+++ commontool/vis/plot.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from scipy.stats import pearsonr
+from sklearn.metrics import r2_score
+from matplotlib import pyplot as plt
+
+
+def polyfit_plot(x, y, deg, scoring='r'):
+    """
+
+    Parameters
+    ----------
+    x :
+    y :
+    deg :
+    scoring :
+
+    References
+    ----------
+    1. https://blog.csdn.net/fffsolomon/article/details/104831050
+    """
+
+    # fit and construct polynomial
+    coefs = np.polyfit(x, y, deg)
+    polynomial = np.poly1d(coefs)
+    print('polynomial:\n', polynomial)
+
+    # scoring
+    y_pred = polynomial(x)
+    if scoring == 'r':
+        score = pearsonr(y, y_pred)[0]
+    elif scoring == 'r2_score':
+        score = r2_score(y, y_pred)
+    else:
+        raise ValueError("Not supported scoring:", scoring)
+    print('\nscore:', score)
+
+    # plot scatter
+    plt.scatter(x, y)
+
+    # plot fitted curve
+    x_min, x_max = np.min(x), np.max(x)
+    x_plot = np.linspace(x_min, x_max, 100)
+    y_plot = polynomial(x_plot)
+    plt.plot(x_plot, y_plot)
